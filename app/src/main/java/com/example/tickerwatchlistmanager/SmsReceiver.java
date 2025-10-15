ee4a28b94b842ce3363fa447e554ea03ab85c801
jonathanp2004 <jpadilla1782.2@gmail.com>
2025-10-14 19:53:24 -0500

    Add SmsReceiver class and update AndroidManifest.xml for SMS permissions

---
+++ app/src/main/java/com/example/tickerwatchlistmanager/SmsReceiver.java
@@ -0,0 +1,15 @@
+package com.example.tickerwatchlistmanager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SmsReceiver extends BroadcastReceiver {
+
+    @Override
+    public void oReceive(Context context, Intent intent) {
+        // Handle the received SMS message here
+    }
+
+
+}
